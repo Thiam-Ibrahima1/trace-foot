0cb816aa461693ee31a40f06528084b1a8e1651f
IBRAHIMA THIAM <IBOU22200@gmail.com>
2026-06-10 11:21:11 +0000

    Force cache driver file

--- backend/config/cache.php
+++ backend/config/cache.php
@@ -1,20 +1,15 @@
 <?php
 return [
-    'default' => env('CACHE_DRIVER', 'file'),
+    'default' => 'file',
     'stores' => [
         'file' => [
             'driver' => 'file',
             'path' => storage_path('framework/cache/data'),
-        ],
-        'database' => [
-            'driver' => 'database',
-            'table' => 'cache',
-            'connection' => null,
         ],
         'array' => [
             'driver' => 'array',
             'serialize' => false,
         ],
     ],
-    'prefix' => env('CACHE_PREFIX', 'trace_fc_cache'),
+    'prefix' => 'trace_fc_cache',
 ];
